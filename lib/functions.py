42f42ffd34e3541aa9a76f7bee4263817665bab7
Duane Grell <duanegrell@gmail.com>
2023-02-23 09:36:40 -0500

    all tests passing

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print ("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print (f"Hello, {name}!")
+
+greet("duane")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print (f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Mel")
+    
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
+    return(num1 + num2)
+
+add(2, 3)
 
 def halve(number):
-    pass
+    print(number/2)
+    return(number/2)
+
+halve(8)
